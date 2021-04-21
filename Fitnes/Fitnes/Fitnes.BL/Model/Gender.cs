413dd142eb2f193fdcd371b8345459c59a79b321
Viktor <vik3224@gmail.com>
2021-04-21 14:35:13 +0300

    Модель User
    Создана модель Пользователя, Гендер

---
+++ Fitnes/Fitnes/Fitnes.BL/Model/Gender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitnes.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+        public Gender (string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя гендера не может быть пустым.", nameof(name));
+            }
+
+            Name = name;
+        }
+    }
+}
